a354e71e49b839b0d9d61c449020a0b733c9a01a
Balthier1337 <raptors.ftw@gmail.com>
2016-07-12 12:38:56 +0100

    v2: General Improvements
    Added "-help"
    Added "-wait"

--- StartMin.ps1
+++ StartMin.ps1
@@ -1,27 +1,33 @@
 # 
 # Author: Joseph McConville
 # Website: http://www.lionheartservices.co.uk
-# Created: 2015-16-02
-# Modified: 2015-16-02
+# Created: 2015-02-16
+# Modified: 2015-02-18
 # Script: Minimize Me
-# Version: 1.0
+# Version: 2.0
 # Description: A quick PowerShell script for starting program in a minimized state.
-# 
 
 [CmdletBinding()]
 param(
+[switch]$help
 [string]$program
+[string]$wait
 )
 
-$psVersion = Get-Host | select version
+if ($help) {
+	echo "Accepted parameters:"
+	echo "-help"
+	echo "Displays current help screen"
+	echo "-program <Program>"
+	echo "Specifies the program to launch"
+	echo "-wait <Number>"
+	echo "Specifies the time period, in seconds, for the program to wait before launching"
+	Break
+}
 
-function startProgram() {
+if ($wait) {
+	Start-Sleep -s $wait
+}
+if ($program) {
 	cmd.exe /c start /b /min $program
-}
-
-if ($psVersion -Like "*2*") {
-	startProgram
-}
-if ($psVersion -Like "*1*") {
-	startProgram
 }
